a8004bd1dfe9b0698b2df209829fff3d38119a34
osama seid <osaseid1@gmail.com>
2024-09-01 09:09:20 +0300

    api integeration

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,3 +5,10 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing book instances.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
