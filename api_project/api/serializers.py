fb866c518c5ba6af7a981b82bb1fb842612a8d67
osama seid <osaseid1@gmail.com>
2024-09-01 09:25:21 +0300

    api integeration

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -5,12 +5,4 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
-        
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-}
+
