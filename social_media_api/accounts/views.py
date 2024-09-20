4a848f2d68911a0d8aabf765eae2d17ee79d48d3
osama seid <osaseid1@gmail.com>
2024-09-20 22:21:55 +0300

    api integeration

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,30 +1,20 @@
-from rest_framework import generics, status
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import get_user_model
-from .serializers import CustomUserSerializer, LoginSerializer
-from django.contrib.auth import authenticate
+from rest_framework import status, permissions
+from rest_framework.decorators import api_view
+from .models import CustomUser
 
-User = get_user_model()
+@api_view(['POST'])
+def follow_user(request, user_id):
+    user_to_follow = get_object_or_404(CustomUser, id=user_id)
+    if request.user == user_to_follow:
+        return Response({"error": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
+    
+    request.user.following.add(user_to_follow)
+    return Response({"message": f"You are now following {user_to_follow.username}"}, status=status.HTTP_200_OK)
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = CustomUserSerializer
-
-    def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        user = User.objects.get(username=response.data['username'])
-        token, _ = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key}, status=status.HTTP_201_CREATED)
-
-class LoginView(generics.GenericAPIView):
-    serializer_class = LoginSerializer
-
-    def post(self, request, *args, **kwargs):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(username=username, password=password)
-        if user:
-            token, _ = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key}, status=status.HTTP_200_OK)
-        return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['POST'])
+def unfollow_user(request, user_id):
+    user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+    request.user.following.remove(user_to_unfollow)
+    return Response({"message": f"You have unfollowed {user_to_unfollow.username}"}, status=status.HTTP_200_OK)
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,33 +1,13 @@
-from rest_framework import viewsets, permissions
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
-from rest_framework import filters
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Post
+from accounts.models import CustomUser
 
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content']
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
+@api_view(['GET'])
+def user_feed(request):
+    followed_users = request.user.following.all()
+    posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+    # Assuming you already have a PostSerializer
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
