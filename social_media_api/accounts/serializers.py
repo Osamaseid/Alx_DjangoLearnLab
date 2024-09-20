8dde9893550c9f708ddf659ca648f165236fe4a1
osama seid <osaseid1@gmail.com>
2024-09-20 22:00:10 +0300

    api integeration

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model  # This imports the custom user model
 from rest_framework.authtoken.models import Token
 
-# Get the custom user model
+# Get the user model (this works with both the default and custom user model)
 User = get_user_model()
 
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -11,15 +11,18 @@ class CustomUserSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'bio', 'profile_picture', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
+    # This method handles the creation of a user and ensures the password is hashed
     def create(self, validated_data):
-        # Create the user using the validated data and ensure password is hashed
+        # Use the create_user method to create the user with a hashed password
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
-            password=validated_data['password']
+            password=validated_data['password'],  # Ensure the password is hashed
+            bio=validated_data.get('bio', ''),  # Optional fields can have default values
+            profile_picture=validated_data.get('profile_picture', None)
         )
         
-        # Create an auth token for the user
+        # Generate and associate a token with the new user
         Token.objects.create(user=user)
         
         return user
@@ -27,4 +30,3 @@ class CustomUserSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
-
