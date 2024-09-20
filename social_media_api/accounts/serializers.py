460af72ca646a79062565a168fbf75939098dda9
osama seid <osaseid1@gmail.com>
2024-09-20 22:03:57 +0300

    api integeration

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,32 +1,32 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model  # This imports the custom user model
+from .models import CustomUser
+from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
-# Get the user model (this works with both the default and custom user model)
-User = get_user_model()
+
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['username', 'email', 'bio', 'profile_picture', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        model = get_user_model()# customUser
+        fields = ('username','bio', 'profile_picture','followers')
 
-    # This method handles the creation of a user and ensures the password is hashed
-    def create(self, validated_data):
-        # Use the create_user method to create the user with a hashed password
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data.get('email'),
-            password=validated_data['password'],  # Ensure the password is hashed
-            bio=validated_data.get('bio', ''),  # Optional fields can have default values
-            profile_picture=validated_data.get('profile_picture', None)
-        )
-        
-        # Generate and associate a token with the new user
-        Token.objects.create(user=user)
-        
-        return user
+# This serializer handles user registration ensures password is write only wont be returned in the response
+class RegisterSerializer(serializers.ModelSerializer):
+    password  = serializers.CharField(required=True, write_only=True)
+    class Meta:
+        model = get_user_model()
+        fields = ('username','email','password')
 
+    def create(self,validated_data):
+            user = get_user_model().objects.create_user( # the create_user handles the password hashing
+                username=validated_data['username'],
+                email=validated_data['email'],
+                password=validated_data['password'],
+
+            )
+            Token.objects.create(user=user) # creating a token for the user after registration
+            return user
+        
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
